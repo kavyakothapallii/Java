ae3eb0bcc3f35e41ace121d27c0f62850ffe7cfb
kavyakothapallii <158547691+kavyakothapallii@users.noreply.github.com>
2024-03-14 12:17:38 +0530

    Create lower.java

---
+++ lower.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        
+        Set<Character> uniqueChars = new TreeSet<>();
+        
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c) || c==' ') {
+                uniqueChars.add(Character.toLowerCase(c));
+            }
+        }
+        
+        System.out.println(uniqueChars);
+    }
+}
