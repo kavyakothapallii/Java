46627a8e1c2aedbcb59c702883bd257f394cf1f4
kavyakothapallii <158547691+kavyakothapallii@users.noreply.github.com>
2024-03-14 12:06:37 +0530

    Create armstrong.java

---
+++ armstrong.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        
+        if(isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number");
+        } else {
+            System.out.println(number + " is not an Armstrong number");
+        }
+    }
+    
+    public static boolean isArmstrong(int num) {
+        int originalNumber, remainder, result = 0, n = 0;
+        originalNumber = num;
+
+        for (originalNumber = num; originalNumber != 0; originalNumber /= 10, ++n);
+
+        originalNumber = num;
+
+        while (originalNumber != 0) {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, n);
+            originalNumber /= 10;
+        }
+
+        return result == num;
+    }
+}
